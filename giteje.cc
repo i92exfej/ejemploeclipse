1fb5e98c814b5331042abfec66475aaefac12cdd
i92exfej <i92exfej@uco.es>
2020-11-24 10:14:07 +0100

    primmer commit

---
+++ giteje.cc
@@ -0,0 +1,20 @@
+/*
+ * ejemplo1.cc
+ *
+ *  Created on: 24 nov. 2020
+ *      Author: JoseAngelExposito
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+cout<<"Hola mundo"<<endl; // @suppress("Symbol is not resolved")
+return 0;
+for(int i=0;i<10;i++) cout<<i;
+}
+
+
+
